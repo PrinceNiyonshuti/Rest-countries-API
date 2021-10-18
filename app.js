5bcea83a89064f54d8c3e554ac170dfbfc7f2c7f
princeniyonshuti <npprince47@gmail.com>
2021-10-19 01:31:34 +0200

    population added commas separator and other missingg info

--- app.js
+++ app.js
@@ -31,17 +31,23 @@ async function searchCountries() {
 }
 
 const displayCountries = (data) => {
-	data.forEach((country) => {
+    data.forEach((country) => {
+        const { flags, name, capital, region, continents, population, currencies, languages } = country;
+        console.log(currencies,languages)
         // result.innerHTML = "";
         const div = document.createElement("div");
-        div.className = "container rounded-lg shadow-lg bg-white ";
+        div.className = "container rounded-lg shadow-lg bg-white pb-4";
         div.innerHTML = `
-                    <img src="${country.flags.svg}" class="h-1/2 w-full rounded-tl-lg rounded-tr-lg" alt="${country.name.common}" />
+                    <img src="${flags.svg}" class="h-1/2 w-full rounded-tl-lg rounded-tr-lg" alt="${name.common}" />
                     <div class="p-4">
-                        <h2 class="text-xl font-bold mb-4">${country.name.common}</h2>
-                        <p class="font-semibold">Population : <span class="text-gray-700 ">${country.population}</span></p>
-                        <p class="font-semibold">Region : <span class="text-gray-700 ">${country.region}</span></p>
-                        <p class="font-semibold">Capital : <span class="text-gray-700 ">${country.capital}</span></p>
+                        <h2 class="text-xl font-bold mb-4">${name.common}</h2>
+                        <p class="font-semibold">Capital : <span class="text-gray-700 ">${capital}</span></p>
+                        <p class="font-semibold">Region : <span class="text-gray-700 ">${region}</span></p>
+                        <p class="font-semibold">Continent : <span class="text-gray-700 ">${continents}</span></p>
+                        <p class="font-semibold">Population : <span class="text-gray-700 ">${population.toLocaleString('en')}</span></p>
+                        <p class="font-semibold">Currency Name : <span class="text-gray-700 ">${currencies.name}</span></p>
+                        <p class="font-semibold">Symbol : <span class="text-gray-700 ">${region}</span></p>
+                        <p class="font-semibold">Main Language : <span class="text-gray-700 ">${languages}</span></p>
                     </div>
         `;
         result.appendChild(div);
@@ -56,4 +62,9 @@ async function filterCountries() {
 	);
 	let data = await response.json();
 	displayCountries(data);
+}
+
+
+function numberWithCommas(x) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
