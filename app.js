f46633ee8949cadda0ee0f09fbb22aad2e32e633
princeniyonshuti <npprince47@gmail.com>
2021-10-19 11:18:10 +0200

    fixed issue of of currencies and symbol

--- app.js
+++ app.js
@@ -15,10 +15,11 @@ filterByRegion.addEventListener("change", filterCountries);
 
 let data = [];
 
+//getting all countries at the start
 async function getCountries() {
 	const response = await fetch("https://restcountries.com/v3.1/all");
 	let data = await response.json();
-	console.log(data);
+	displayCountries(data);
 }
 
 // Searching Country
@@ -31,28 +32,40 @@ async function searchCountries() {
 }
 
 const displayCountries = (data) => {
-    result.innerHTML = "";
-    data.forEach((country) => {
-        const { flags, name, capital, region, continents, population, currencies, languages } = country;
-        const div = document.createElement("div");
-        div.className = "container rounded-lg shadow-lg bg-white pb-4";
+	result.innerHTML = "";
+	data.forEach((country) => {
+		const {
+			flags,
+			name,
+			capital,
+			region,
+			continents,
+			population,
+			currencies,
+			languages,
+        } = country;
+        const currencyNames = currencies[Object.keys(currencies)[0]];
+        const langNames = currencies[Object.keys(languages)[0]];
+        console.log(languages);
+		const div = document.createElement("div");
+		div.className =
+			"w-full mb-4  sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 rounded container rounded-lg shadow-lg bg-white pb-4";
         div.innerHTML = `
                     <img src="${flags.svg}" class="h-1/2 w-full rounded-tl-lg rounded-tr-lg" alt="${name.common}" />
-                    <div class="p-4">
+                    <div class="p-4 h-auto">
                         <h2 class="text-xl font-bold mb-4">${name.common}</h2>
                         <p class="font-semibold">Capital : <span class="text-gray-700 ">${capital}</span></p>
                         <p class="font-semibold">Region : <span class="text-gray-700 ">${region}</span></p>
                         <p class="font-semibold">Continent : <span class="text-gray-700 ">${continents}</span></p>
-                        <p class="font-semibold">Population : <span class="text-gray-700 ">${population.toLocaleString('en')}</span></p>
-                        <p class="font-semibold">Currency Name : <span class="text-gray-700 ">${currencies[0]}</span></p>
-                        <p class="font-semibold">Symbol : <span class="text-gray-700 ">${region}</span></p>
+                        <p class="font-semibold">Population : <span class="text-gray-700 ">${population.toLocaleString("en")}</span></p>
+                        <p class="font-semibold">Currency Name : <span class="text-gray-700 ">${currencyNames.name}</span></p>
+                        <p class="font-semibold">Symbol : <span class="text-gray-700 ">${currencyNames.symbol}</span></p>
                         <p class="font-semibold">Main Language : <span class="text-gray-700 ">${languages}</span></p>
                     </div>
         `;
-        result.appendChild(div);
+		result.appendChild(div);
 	});
 };
-
 
 // Filter countries by region Done
 async function filterCountries() {
@@ -61,9 +74,4 @@ async function filterCountries() {
 	);
 	let data = await response.json();
 	displayCountries(data);
-}
-
-
-function numberWithCommas(x) {
-	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
